e73cbc7a352c4482c6ab5879ccdd456669e6f9d6
BonesTheGhost <bonestheghost@gmail.com>
2020-01-11 10:53:25 -0600

    [?] UPDATE:: Attempted to add the heroku MySQL code for a JawsDB add-on in order to get the app to run properly...

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,27 @@
 const mysql = require('mysql');
+const connection;
+
+//OLD CONNECTION THAT WORKS LOCALLY
+/*
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'titanus2242',
     database: 'burgers_db'
 });
+*/
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'titanus2242',
+        database: 'burgers_db'
+    })
+}
 
 
 connection.connect();
-
 module.exports = connection;
